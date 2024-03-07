bfdb4b32fcfad3a41bed511c5ecd1d2b05fd0592
CrioUser <crio-user@criodo.com>
2024-03-07 23:02:47 +0530

    ME_GEEKTRUST_BACKEND

--- src/main/java/com/geektrust/backend/App.java
+++ src/main/java/com/geektrust/backend/App.java
@@ -1,13 +1,10 @@
-  package com.geektrust.backend;
+package com.geektrust.backend;
 
-import java.io.BufferedReader;
+
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
-import com.geektrust.backend.concrete.CourseSchedulerSystem;
-import com.geektrust.backend.exception.InvalidInputException;
-import com.geektrust.backend.models.Command;
-import com.geektrust.backend.service.CommandService;
+
 import com.geektrust.backend.service.FileInputReader;
 
 
